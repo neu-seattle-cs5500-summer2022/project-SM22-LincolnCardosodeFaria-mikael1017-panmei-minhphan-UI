623b2f789483c25e93f71bc5e70068b5e08152f0
panmq122 <pan.mei@northeastern.edu>
2022-08-02 14:45:09 -0700

    deploy test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
+        <Route exact path={"/"} element={<Homepage />}></Route>
         {/* <Route path="signup" element={<SignupPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path={"client/:id"} element={<ClientPage />} />
